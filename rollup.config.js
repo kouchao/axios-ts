9fc3d3fa7537fd9f31762f89721ec8b0653d5215
kouchao <admin@jskou.com>
2020-05-27 11:25:54 +0800

    feat: terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import ts from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
 
 const formats = ['umd', 'esm']
 const output = formats.map((format) => ({
-  file: `dist/bundle.${format}.js`,
+  file: `dist/axios-ts.${format}.min.js`,
   format,
   name: 'axiosTs',
 }))
@@ -13,6 +15,8 @@ export default {
   output,
   plugins: [
     resolve(),
+    commonjs(),
     ts(),
+    terser()
   ],
 }
